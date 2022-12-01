cd8243cfa2a9026af525eca4d55c2f4d4ad6fe73
Manav Misra <manavm@visionwebsoft.com>
2022-11-30 19:40:17 -0600

    Add custom error route

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Error from "./routes/Error";
 import Home from "./routes/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <Error />,
   },
 ]);
 

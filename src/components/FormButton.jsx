2d330cf5c805e8e32a805d05a05bd7afbb4aca0b
Manav Misra <manavm@visionwebsoft.com>
2022-11-30 19:39:37 -0600

    Add basic styles

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -1,7 +1,12 @@
 export default function FormButton() {
   return (
     <form method="POST">
-      <button type="submit">Submit</button>
+      <button
+        type="submit"
+        className="flex h-8 items-center rounded bg-slate-100 p-2 text-sky-500"
+      >
+        New
+      </button>
     </form>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,15 @@
 export default function Form() {
   return (
     <form>
-      <label htmlFor="search">Search</label>
-      <input id="search" type="search" />
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <input
+        id="search"
+        type="search"
+        placeholder="Search"
+        className="h-8 rounded p-1"
+      />
     </form>
   );
 }
